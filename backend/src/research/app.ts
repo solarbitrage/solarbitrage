1fe255593a66402e49591e5101ce5e45eca6fda2
ryancorridor <ryan.hall.tx@gmail.com>
2022-02-16 00:40:36 -0600

    Added commands for querying Orca

--- backend/src/research/app.ts
+++ backend/src/research/app.ts
@@ -1,11 +1,57 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import { readFile } from "mz/fs";
+import { Connection, Keypair } from "@solana/web3.js";
+import { getOrca, OrcaFarmConfig, OrcaPoolConfig, Network } from "@orca-so/sdk";
+import Decimal from "decimal.js";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const main = async () => {
+  /*** Setup ***/
+  // 1. Read secret key file to get owner keypair
+  const secretKeyString = await readFile("wallets/test-keypair.json", {
+    encoding: "utf8",
+  });
+  const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+  const owner = Keypair.fromSecretKey(secretKey);
 
-app.listen(port, () => {
-    return console.log('Express is listening at http://localhost:${port}');
-})
+  // 2. Initialzie Orca object with mainnet connection
+  // Production Settings
+  //const connection = new Connection("https://api.mainnet-beta.solana.com", "singleGossip");
+  //const orca = getOrca(connection);
+   
+  // DevNet Settings
+  const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
+  const orca = getOrca(connection, Network.DEVNET)
+
+  try {
+    /*** Swap ***/
+    // 3. We will be swapping 0.1 SOL for some ORCA
+    const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
+    const solToken = orcaSolPool.getTokenB();
+    const solAmount = new Decimal(0.1);
+    const quote = await orcaSolPool.getQuote(solToken, solAmount);
+    console.log(quote);
+    console.log(quote.getExpectedOutputAmount().toDecimal());
+    console.log(quote.getLPFees().toDecimal());
+    console.log(quote.getMinOutputAmount().toDecimal());
+    console.log(quote.getNetworkFees().toDecimal());
+    console.log(quote.getPriceImpact());
+    console.log(quote.getRate());
+
+    /*const orcaAmount = quote.getMinOutputAmount();
+
+    console.log(`Swap ${solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA`);
+    const swapPayload = await orcaSolPool.swap(owner, solToken, solAmount, orcaAmount);
+    const swapTxId = await swapPayload.execute();
+    console.log("Swapped:", swapTxId, "\n");
+    */
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+main()
+  .then(() => {
+    console.log("Done");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
