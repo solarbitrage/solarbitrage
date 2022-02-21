dd8fa9871e7b1da211e682717716ede2c21d33b6
ryancorridor <ryan.hall.tx@gmail.com>
2022-02-20 18:40:30 -0600

    Added support for Firebase RT-DB

--- backend/data_collection/src/research/app.ts
+++ backend/data_collection/src/research/app.ts
@@ -3,8 +3,8 @@ import { Connection, Keypair } from "@solana/web3.js";
 import { getOrca, OrcaPoolConfig, Network } from "@orca-so/sdk";
 import Decimal from "decimal.js";
 
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp, deleteApp } from "firebase/app";
+import { getDatabase, ref, set, update } from "firebase/database";
 
 import config from "./config"
 
@@ -23,26 +23,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-const main = async () => {
-  /*** Setup ***/
-  // 1. Read secret key file to get owner keypair
+const orcaRequests = async () => {
+  // Read secret key file to get owner keypair
   const secretKeyString = await readFile("wallets/test-keypair.json", {
     encoding: "utf8",
   });
   const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
   const owner = Keypair.fromSecretKey(secretKey);
 
-  // Initialzie Orca object with mainnet connection
-
-  // Production Settings
-  //const connection = new Connection("https://api.mainnet-beta.solana.com", "singleGossip");
-  //const orca = getOrca(connection);
-   
-  // DevNet Settings
-  const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
-  const orca = getOrca(connection, Network.DEVNET)
+  // Initialzie Orca object with appropriate network connection
+  // Production: https://api.mainnet-beta.solana.com, getOrca(connection)
+  // Development: https://api.devnet.solana.com, getOrca(connection, Network.DEVNET)
+  const connection = new Connection("https://api.mainnet-beta.solana.com", "singleGossip");
+  const orca = getOrca(connection)
 
   try {
+    console.log('Gathering ORCA data')
     const pools = [
       OrcaPoolConfig.SOL_USDC,
     ]
@@ -54,23 +50,38 @@ const main = async () => {
       const coinToken = currentPool.getTokenA();
       const usdcToken = currentPool.getTokenB();
 
-      const tradingAmount = new Decimal(0.1);
+      const tradingAmount = new Decimal(1);
       const buyQuote = await currentPool.getQuote(usdcToken, tradingAmount);
       const sellQuote = await currentPool.getQuote(coinToken, tradingAmount);
 
-      console.log('Swap ' + tradingAmount + ' USDC for at least ' + buyQuote.getMinOutputAmount().toNumber() + ' SOL')
-      console.log('Swap ' + tradingAmount + ' SOL for at least ' + sellQuote.getMinOutputAmount().toNumber() + ' USDC')
-
       // Update Firebase Real-time Database
-      //const poolName = Object.keys(OrcaPoolConfig).find(key => OrcaPoolConfig[key] === pool)
+      const poolName = Object.keys(OrcaPoolConfig).find(key => OrcaPoolConfig[key] === pool)
+      updateDatabase('ORCA_' + poolName + '_BUY', buyQuote);
+      updateDatabase('ORCA_' + poolName + '_SELL', sellQuote);
 
+      console.log('Update complete')
+      console.log('Waiting until next call')
+      console.log('\n')
+
+      setTimeout(orcaRequests, 5000)
     })
   } catch (err) {
     console.warn(err);
   }
 };
 
-main()
+function updateDatabase(poolName, quote) {
+  set(ref(database, 'latest_prices/' + poolName), {
+    expected_output_amount: quote.getExpectedOutputAmount().toNumber(),
+    lp_fees: quote.getLPFees().toNumber(),
+    min_output_amount: quote.getMinOutputAmount().toNumber(),
+    network_fees: quote.getNetworkFees().toNumber(),
+    price_impact: quote.getPriceImpact().toNumber(),
+    rate: quote.getRate().toNumber()
+  });
+}
+
+orcaRequests()
   .then(() => {
     console.log("Done");
   })
