0f789c49d9f82d4365738f8f282aad9971507cca
Noel <noel_115@tamu.edu>
2022-02-28 11:46:51 -0600

    Cleaned up bot.ts

--- backend/bot/src/bot.ts
+++ backend/bot/src/bot.ts
@@ -55,7 +55,7 @@ run_pools().then((queries)=>{
   // console.log(queries[ORCA_USDC_SELL]);
   console.log("init query");
   local_database = queries;
-  calculate();
+  calculate_trade();
 });
 
 
@@ -70,21 +70,13 @@ onChildChanged(starCountRef, (snapshot) => {
   console.log('\n ------------------------------- \n')
 
   //function that runs caclculations anytime there's a change
-  calculate(snapshot.key);
+  calculate_trade(snapshot.key);
 });
-// --------------------------------------test------------------------------------------------------------
+// calculate function
 
-// var intervalID = setInterval(run_pools, 6000);
-
-
-
-function calculate(update?){
+function calculate_trade(update?){
 // console.log(local_database, update)
-  console.log("we calculating")
-
-  //find triange
-  //start usdc
-  //come back to usdc
+  console.log("Calculating ...")
 
 //   let fromUSDC = Object.entries(local_database).filter(([key, value]) =>{
 //    return value.from === 'USDC'
@@ -96,147 +88,48 @@ function calculate(update?){
 // })
 
   let rates = []
-  console.log(local_database);
+  // console.log(local_database);
   rates[0] = local_database.ORCA_SOL_USDC_BUY.rate;
   rates[1] = local_database.ORCA_SOL_USDC_SELL.rate;
   rates[2] = local_database.RAYDIUM_SOL_USDC_SELL.rate;
 
-  if(local_database.ORCA_SOL_USDC_SELL.rate !==  local_database.RAYDIUM_SOL_USDC_SELL.rate){
-    //hacked the system
-    //buy
-    // multiply usdc * rate of orca
-    // trade based on a threshold of 
-    // console.log("estimated profit per coin", Math.abs((local_database.ORCA_SOL_USDC_SELL.rate -  local_database.RAYDIUM_SOL_USDC_SELL.rate)));
-    console.log("Buy SOL with USDC from", (local_database.ORCA_SOL_USDC_SELL.rate > local_database.RAYDIUM_SOL_USDC_SELL.rate) ? "Orca":"Raydium");
-    let usdc = 1;  // base value of $1
+  // have rate_diff? don't trade till above base value?
+  // rn calculate() called at any update in database -> not on sol_usdc pool of Raydium and Orca.
+  // 
+  let buy_from:string, sell_to:string;
+  let usdc = 1;  // base value of $1
+  let rate_diff = Math.abs(local_database.ORCA_SOL_USDC_SELL.rate - local_database.RAYDIUM_SOL_USDC_SELL.rate);
+  if(rate_diff > 0.1){
     if(local_database.ORCA_SOL_USDC_SELL.rate < local_database.RAYDIUM_SOL_USDC_SELL.rate){
       // buy sol from ORCA and sell sol to get usdc 
-      usdc = usdc*local_database.RAYDIUM_SOL_USDC_SELL.rate;
-      usdc = usdc*local_database.ORCA_SOL_USDC_BUY.rate;
+      usdc = usdc*local_database.RAYDIUM_SOL_USDC_BUY.rate;
+      usdc = usdc*local_database.ORCA_SOL_USDC_SELL.rate;   
+      buy_from = "RAYDIUM_SOL_USDC";
+      sell_to = "ORCA_SOL_USDC";
+      console.log("Buy from Raydium, Sell to Orca");
     }
     else{
-      usdc = usdc*local_database.ORCA_SOL_USDC_SELL.rate;
-      usdc = usdc*local_database.RAYDIUM_SOL_USDC_BUY.rate;
+      usdc = usdc*local_database.ORCA_SOL_USDC_BUY.rate;
+      usdc = usdc*local_database.RAYDIUM_SOL_USDC_SELL.rate;
+      buy_from = "ORCA_SOL_USDC";
+      sell_to = "RAYDIUM_SOL_USDC";
+      console.log("Buy from Orca, Sell to Raydium");
     }
     console.log("Resulting USDC amount: " , usdc);
-
-  //   addDoc(collection(firestore, "profitability_evaluation_history"), {
-  //     // from, to, amm, timestamp
-  // })
   }
-  // console.log(fromUSDC, "frm usd")
-}
+  else{
+    console.log("Not worth the trade\n");
+  }
 
-// console.log(p.ORCA_USDC_SELL);
-
-
-// --------------------------------------test------------------------------------------------------------
-
-// const main = async () => {
-//   /*** Setup ***/
-//   // 1. Read secret key file to get owner keypair
-//   const secretKeyString = await readFile("/Users/noelb/my-solana-wallet/my-keypair.json", {
-//       encoding: "utf8",
-//     });
-//     // "6VgdQS12EqfS31LkG9ksveqQQAJjiVbZ2F7rMzc8Cdec"
-//   const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
-//   const owner = Keypair.fromSecretKey(secretKey);
-
-//   // 2. Initialzie Orca object with devnet connection
-//   const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
-//   const orca = getOrca(connection, Network.DEVNET);
-
-//   try {
-//     const pools = [
-//       OrcaPoolConfig.SOL_USDC,
-//       OrcaPoolConfig.ORCA_SOL,
-//       OrcaPoolConfig.ORCA_USDC
-//     ]
-//     const query_pools = ['SOL_USDC_BUY/', 'ORCA_SOL_BUY/', 'ORCA_USDC_SELL/'];
-
-//     // Getting USDC from SOL wallet
-//     const solUSDCPool = orca.getPool(OrcaPoolConfig.SOL_USDC);
-//     const solTokensell = solUSDCPool.getTokenA();
-//     const solAmountsell = new Decimal(0.011139);  // getting $1
-//     const solquotesell = await solUSDCPool.getQuote(solTokensell, solAmountsell);
-//     const USDCAmountbuy = solquotesell.getMinOutputAmount();
-    
-//     console.log(`Swapertoto ${solAmountsell.toString()} SOL for at least ${USDCAmountbuy.toNumber()} USDC`);
-//     const swap = await solUSDCPool.swap(owner, solTokensell, solAmountsell, USDCAmountbuy);
-//     const swapped = await swap.execute();
-//     console.log("\n swap id: ", swapped)
-//     // Getting USDC from SOL wallet
-
-//     // const orcaUSDCPool = orca.getPool(OrcaPoolConfig.ORCA_USDC);
-//     // const USDCToken = orcaUSDCPool.getTokenB();
-//     // const USDCAmount = new Decimal(1);
-//     // const USDCquote = await orcaUSDCPool.getQuote(USDCToken, USDCAmount);
-//     // const USDCorcaAmount = USDCquote.getMinOutputAmount();
-
-//     // const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
-//     // const solToken = orcaSolPool.getTokenB();
-//     // const solAmount = new Decimal(0.1);
-//     // const solquote = await orcaSolPool.getQuote(solToken, solAmount);
-//     // const orcaAmount = solquote.getMinOutputAmount();
-
-//     // console.log(`Swapertoto ${solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA`);
-//     // const swapPayload = await orcaSolPool.swap(owner, solToken, solAmount, orcaAmount);
-//     // const swapTxId = await swapPayload.execute();
-
-//     // console.log("Swapped:", swapTxId, "\n");
-
-//   } catch (err) {
-//     console.warn(err);
-//   }
-// };
-
-// main()
-//   .then(() => {
-//     console.log("Done");
-//   })
-//   .catch((e) => {
-//     console.error(e);
-//   });
-
-
-// const latest_price = ref(database, 'latest_prices/ORCA_'+'ORCA_SOL_USDC_BUY/');
-// get(latest_price).then((snapshot)=>{
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// }).catch((error)=>{
-//   console.error(error);
-// });
-// onValue(latest_price, (snapshot) => {
-  // const data = snapshot.val().key;
-  // console.log(snapshot.size, snapshot.val());
-  // snapshot.forEach(snap => {
-  //   console.log(snap.val());
-  // })
-//   updateStarCount(postElement, data);
-// });
-
-// const dbRef = ref(getDatabase());
-// get(child(dbRef, `users/${userId}`)).then((snapshot) => {
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// }).catch((error) => {
-//   console.error(error);
-// });
-
-
-
-// var intervalID = setInterval(myCallback, 5000, 'Parameter 1', 'Parameter 2');
-
-// function myCallback(a, b)
-// {
-//  // Your code here
-//  // Parameters are purely optional.
-//  console.log(a);
-//  console.log(b);
-// }
+  // write to firestore
+  // try {
+  //   const docRef = await addDoc(collection(firestore, "profitability_evaluation_history"), {
+  //     buy_from: buy_from,
+  //     sell_to: sell_to,
+  //     net_profit_rate: usdc,
+  //     time_stamp: serverTimestamp()
+  //   });
+  //   console.log("Document written with ID: ", docRef.id);
+  // } catch (e) {
+  //   console.error("Error adding document: ", e);
+  // }
