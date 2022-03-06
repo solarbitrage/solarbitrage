5b404949903cafcf6d0262c769a9aa685304390e
Daniel Phan <phand972@gmail.com>
2022-03-06 17:44:34 -0600

    added a chart to metrics

--- frontend/src/pages/Metrics.js
+++ frontend/src/pages/Metrics.js
@@ -1,5 +1,36 @@
+import React from "react";
+import PriceHistoryPlot from "../components/dashboard/pricingHistoryPlot";
+
 function Metrics() {
-    return <h1>Metrics</h1>
+  return (
+    <div className="metric">
+      <div className="page-container">
+        <h1>Metrics</h1>
+
+        <div className="widget-container">
+          <PriceHistoryPlot
+						data={[
+							{
+								type: "scatter",
+								mode: "lines+points",
+								x: [0, 1, 2, 3, 4, 5],
+								y: [0, 1, 2, 3, 4, 5]
+							}
+						]}
+						layout = {
+							{
+								width: 950, 
+								height: 460, 
+								title: "Sell Rates over time"
+							}
+						}
+					/>  
+          
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Metrics
