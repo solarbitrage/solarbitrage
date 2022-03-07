1e8fa3a3ff675b3dcbc3e2d9e680828f792c42e9
Daniel Phan <phand972@gmail.com>
2022-03-06 23:04:45 -0600

    adjusted alignment of containers

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -24,74 +24,79 @@ function Dashboard() {
 	<div className="dashboard">
 		<div className="page-container">
 		<h1>Dashboard</h1>
-			<div className="widget-container">
-				<div className="user-metric">
-					<h2>Current</h2>
-					<h2>Insert Money Here</h2>
+			<div className="user-stats">
+				<div className="widget-container">
+					<div className="user-metric">
+						<h2>Current</h2>
+						<h2>Insert Money Here</h2>
+					</div>
+					<div className="user-metric">
+						<h2>Earnings / Day</h2>
+						<h2>0.0323 USDC</h2>
+					</div>
+					<div className="user-metric">
+						<h2>Bots running</h2>
+						<h2>2/4</h2>
+					</div>
 				</div>
-				<div className="user-metric">
-					<h2>Earnings / Day</h2>
-					<h2>0.0323 USDC</h2>
-				</div>
-				<div className="user-metric">
-					<h2>Bots running</h2>
-					<h2>2/4</h2>
+				
+				<div className="widget-container">
+					<div className="earnings-over-time-plot">
+						<PriceHistoryPlot
+							data={[
+								{
+									type: "scatter",
+									mode: "lines+points",
+									x: pricingHistory.map(ph => new Date(ph.data.timestamp.seconds * 1000)),
+									y: pricingHistory.map(ph => ph.data.rate)
+								}
+							]}
+							layout = {
+								{
+									width: 950, 
+									height: 460, 
+									title: "Monies over time"
+								}
+							}
+						/>
+					</div>
 				</div>
 			</div>
 			
-			<div className="widget-container">
-				<div className="earnings-over-time-plot">
-					<PriceHistoryPlot
-						data={[
-							{
-								type: "scatter",
-								mode: "lines+points",
-								x: pricingHistory.map(ph => new Date(ph.data.timestamp.seconds * 1000)),
-								y: pricingHistory.map(ph => ph.data.rate)
-							}
-						]}
-						layout = {
-							{
-								width: 950, 
-								height: 460, 
-								title: "Monies over time"
-							}
-						}
-					/>
-				</div>
-			</div>
 			
-			<div className="widget-container">
-				<div className="bot-metrics">
-					<h2>Bots</h2>
-					<BotInformation 
-						name="George"
-						strategyUsing="Sub Par"
-						averageEarnings="-0.05%"
-						amms={["Orca", "Raydium"]}
-						currencies={["Solana", "USDC"]}
-					/>
-					<BotInformation
-						name="Daniel"
-						strategyUsing="The Best"
-						averageEarnings="1000%"
-						amms = {["Dan"]}
-						currencies = {["Me"]}
-					/>
-					<BotInformation
-						name="Ryan"
-						strategyUsing="Something"
-						averageEarnings="0%"
-						amms = {["Dan"]}
-						currencies = {["Me"]}
-					/>
-					<BotInformation
-						name="Noel"
-						strategyUsing="Abra"
-						averageEarnings="Kadabra"
-						amms = {["Doge"]}
-						currencies = {["Coin"]}
-					/>
+			<div className="bot-stats">
+				<div className="widget-container">
+					<div className="bot-metrics">
+						<h2>Bots</h2>
+						<BotInformation 
+							name="George"
+							strategyUsing="Sub Par"
+							averageEarnings="-0.05%"
+							amms={["Orca", "Raydium"]}
+							currencies={["Solana", "USDC"]}
+						/>
+						<BotInformation
+							name="Daniel"
+							strategyUsing="The Best"
+							averageEarnings="1000%"
+							amms = {["Dan"]}
+							currencies = {["Me"]}
+						/>
+						<BotInformation
+							name="Ryan"
+							strategyUsing="Something"
+							averageEarnings="0%"
+							amms = {["Dan"]}
+							currencies = {["Me"]}
+						/>
+						<BotInformation
+							name="Noel"
+							strategyUsing="Abra"
+							averageEarnings="Kadabra"
+							amms = {["Doge"]}
+							currencies = {["Coin"]}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
