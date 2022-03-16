dca02dd780e217f8a2a91bcf5d9638b7f201b7b3
Daniel Phan <phand972@gmail.com>
2022-03-16 14:38:22 -0500

    Made metrics page query data only once

--- frontend/src/pages/Metrics.js
+++ frontend/src/pages/Metrics.js
@@ -2,45 +2,67 @@ import React from "react";
 import PriceHistoryPlot from "../components/dashboard/pricingHistoryPlot";
 import Checkbox from "../components/checkbox";
 
-import { collection, query, where, onSnapshot, limit, orderBy } from "firebase/firestore";
+import { collection, query, where, limit, orderBy, getDocs } from "firebase/firestore";
 import database from "../firestore.config";
 
 function Metrics() {  
-  // Checkbox
   const pools = ["ORCA_SOL_USDC_BUY", "RAYDIUM_SOL_USDC_BUY"];
+  const [poolDatas, setPoolDatas] = React.useState(new Array(pools.length).fill(null));
   const [checkedState, setCheckedState] = React.useState(new Array(pools.length).fill(false));
+
+  /**
+   * Fetches data and then updates poolDatas, which will automatically 
+   * update the metrics graph when changed.
+   * @param {string} pool_id id of the pool as listed in firestore
+   * @param {number} index index of pool_id in the pools array
+   */
+  const fetchData = async(pool_id, index) => {
+    const q = query(collection(database, "pricing_history"), where("pool_id", "==", pool_id), orderBy("timestamp", "desc"), limit(10));
+    const docSnap = await getDocs(q);
+
+    // Saves existing data and updates new data.
+    setPoolDatas(existingData => {
+      return existingData.map((poolItem, j) => {
+        if (index === j) {
+          poolItem = docSnap.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+          }))
+        }
+        return poolItem;
+      })
+    })
+  }
+
+  /**
+   * Fetches data if the checkbox in the corresponding position is true.
+   * If the checkbox turns false, removes data from poolDatas.
+   * @param {number} position the index of the checkbox. 
+   */
   const handleCheckboxOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    )
+    const updatedCheckedState = checkedState.map((item, index) => {
+      if (index === position) {
+        item = !item;
+
+        if (item) {
+          fetchData(pools[index], index);
+        } else {
+          setPoolDatas(existingData => {
+            return existingData.map((poolItem, j) => {
+              if (index === j) {
+                poolItem = null;
+              }
+              return poolItem;
+            })
+          })
+        }
+      }
+      return item;
+    })
     console.log(updatedCheckedState);
     setCheckedState(updatedCheckedState);
   }
 
-  const [poolDatas, setPoolDatas] = React.useState(new Array(pools.length).fill(null));
-  React.useEffect(() => {
-    const pricingRef = collection(database, "pricing_history");
-    for (let i = 0; i < pools.length; ++i) {
-      console.log(pools[i]);
-      const q = query(pricingRef, where("pool_id", "==", pools[i]), orderBy("timestamp", "desc"), limit(10));
-      onSnapshot(q, (querySnapshot) => {
-        setPoolDatas(existingData => {
-          return existingData.map((item, j) => {
-            if (i === j) {
-              console.log("ITEM UPDATING");
-              console.log(item);
-              item = querySnapshot.docs.map(doc => ({
-                id: doc.id,
-                data: doc.data()
-              }))
-              console.log(item);
-            }
-            return item;
-          })
-        })
-      })
-    }
-  }, [setPoolDatas]);
 
   React.useEffect(() => {
     console.log(poolDatas);
