db1f309b7b57a32f26f874f7163b8bd48c693397
ryan-hall-tx <ryan.hall.tx@gmail.com>
2022-03-20 11:48:15 -0500

    Added OrcaSwapError for OrcaSwapInstruction

--- backend/on-chain/src/error.rs
+++ backend/on-chain/src/error.rs
@@ -1,1 +1,19 @@
 // error.rs
+use thiserror::Error;
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum OrcaSwapError {
+    /// Invalid Instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    /// Not Rent Exempt
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+}
+
+impl From<OrcaSwapError> for ProgramError {
+    fn from(e: OrcaSwapError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
