6bd310e7b7171819b6f724c5e6ede561380bdf48
ryan-hall-tx <ryan.hall.tx@gmail.com>
2022-03-23 22:09:38 -0500

    Updated library file with files from design

--- backend/on-chain/data-collection/src/lib.rs
+++ backend/on-chain/data-collection/src/lib.rs
@@ -1,8 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+// lib.rs
+pub mod entrypoint;
+pub mod error;
+pub mod instruction;
+pub mod processor;
