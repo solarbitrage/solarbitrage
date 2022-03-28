94eed3766d30a0e0e0f0ac7311ad7501af8e336e
ryan-hall-tx <ryan.hall.tx@gmail.com>
2022-03-28 14:31:47 -0500

    Updated route and added sorting

--- backend/data_collection/src/utils/src/common-pools.ts
+++ backend/data_collection/src/utils/src/common-pools.ts
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 
 const ORCA_API_POOL_ENDPOINT = "https://api.orca.so/allPools";
-const RAYDIUM_API_POOL_ENDPOINT = "https://api.raydium.io/pools";
+const RAYDIUM_API_POOL_ENDPOINT = "https://api.raydium.io/pairs";
 
 const FILTER_COIN = "USDC"
 
@@ -14,7 +14,7 @@ const enum poolAMM {
 function filterPoolName(data, coin: string, amm: poolAMM) {
     return (amm == poolAMM.Orca) ?
         Object.keys(data).filter(k => k.includes(coin)).map(x => x.replace('[aquafarm]', '').replace('[stable]', '')) :
-        data.filter(e => e.identifier.includes(coin)).map(x => x.identifier.replace('-', '/'));
+        data.filter(e => e.name.includes(coin)).sort((p1, p2) => p1.volume_24h * p1.price - p2.volume_24h * p2.price).reverse().map(x => x.name.replace('-', '/'));
 }
 
 (async () => {
