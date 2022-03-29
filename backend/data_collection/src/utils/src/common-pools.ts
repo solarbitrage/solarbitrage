cbb9ac5f4c2d677f0d9b55b047360651570203ce
ryan-hall-tx <ryan.hall.tx@gmail.com>
2022-03-29 15:53:23 -0500

    Small fixes

--- backend/data_collection/src/utils/src/common-pools.ts
+++ backend/data_collection/src/utils/src/common-pools.ts
@@ -13,13 +13,13 @@ const enum poolAMM {
 // Filters pools by name for specific coin
 function filterPoolName(data, coin: string, amm: poolAMM) {
     return (amm == poolAMM.Orca) ?
-        Object.keys(data).filter(k => k.includes(coin)).map(x => x.replace('[aquafarm]', '').replace('[stable]', '')) :
+        [Object.keys(data).filter(k => k.includes(coin)).map(x => x.replace('[aquafarm]', '').replace('[stable]', ''))] :
         data.filter(e => e.name.includes(coin)).sort((p1, p2) => p1.volume_24h * p1.price - p2.volume_24h * p2.price).reverse().map(x => x.name.replace('-', '/'));
 }
 
 (async () => {
     const orcaPoolsRaw = await fetch(ORCA_API_POOL_ENDPOINT).then((res) => res.json());
-    const orcaPools = filterPoolName(orcaPoolsRaw, FILTER_COIN, poolAMM.Orca);
+    const orcaPools = filterPoolName(orcaPoolsRaw, FILTER_COIN, poolAMM.Orca)[0];
 
     const raydiumPoolsRaw = await fetch(RAYDIUM_API_POOL_ENDPOINT).then(res => res.json());
     const raydiumPools = filterPoolName(raydiumPoolsRaw, FILTER_COIN, poolAMM.Raydium)
@@ -27,6 +27,7 @@ function filterPoolName(data, coin: string, amm: poolAMM) {
     const commonPools = new Set(orcaPools.filter(function(e) {
         return raydiumPools.indexOf(e) >= 0;
     }));
+
     
     console.log(commonPools)
 })().catch((e) => console.error(e));
