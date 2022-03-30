850244e5c0029bfe63a9961c290412b9fa356d19
Daniel Phan <phand972@gmail.com>
2022-03-29 19:22:53 -0500

    Best trades algorithm working

--- frontend/src/pages/Metrics.js
+++ frontend/src/pages/Metrics.js
@@ -25,6 +25,8 @@ function Metrics() {
   const [rateDatas, setRateDatas] = React.useState(new Array(ammCheckedState.length * poolCheckedState.length * directionCheckedState.length).fill(null));
   const [rateDisplays, setRateDisplays] = React.useState(new Array(ammCheckedState.length * poolCheckedState.length * directionCheckedState.length).fill(null));
 
+  const [profitDatas, setProfitDatas] = React.useState(new Array(0).fill(null));
+
   /**
    * Fetches data based on which filters are checked.
    */
@@ -34,7 +36,6 @@ function Metrics() {
         for (let directionIndex = 0; directionIndex < directions.length; directionIndex++) {
           if (ammCheckedState[ammIndex] && poolCheckedState[poolIndex] && directionCheckedState[directionIndex]) {
             let poolID = amms[ammIndex] + "_" + pools[poolIndex];
-            console.log(poolID);
             const q = query(collection(database, "pricing_history"), 
               where("pool_id", "==", poolID), 
               where("direction", "==", directions[directionIndex]), 
@@ -155,16 +156,75 @@ function Metrics() {
     calculateProfitability(rateDatas[0], rateDatas[1], rateDatas[12], rateDatas[13], 0);
   }, [rateDatas])
 
-  const calculateProfitability = (poolABuy, poolASell, poolBBuy, poolBSell, transasctionFee) => {
-    console.log({poolABuy, poolASell, poolBBuy, poolBSell, transasctionFee});
-    let estimatedProfits = [];
-    
+  React.useEffect(() => {
+    console.log(profitDatas);
+  }, [profitDatas])
 
-    // let estimatedProfits = {
-    //   "Raydium then Orca": ((1 * local_database.RAYDIUM_SOL_USDC.buy.rate) * local_database.ORCA_SOL_USDC.sell.rate    * (1 - SLIPPAGE)) - 1,
-    //   "Orca then Raydium": ((1 * local_database.ORCA_SOL_USDC.buy.rate) * local_database.RAYDIUM_SOL_USDC.sell.rate * (1 - SLIPPAGE)) - 1
-    // }
-    // console.log("Estimated Profit", estimatedProfits, "Rates", { "Raydium then Orca": [local_database.RAYDIUM_SOL_USDC.buy.rate, local_database.ORCA_SOL_USDC.sell.rate], "Orca then Raydium": [local_database.ORCA_SOL_USDC.buy.rate, local_database.RAYDIUM_SOL_USDC.sell.rate]})
+  const calculateProfitability = (poolABuy, poolASell, poolBBuy, poolBSell, transasctionFee) => {
+    
+    if (!poolABuy || !poolASell || !poolBBuy || !poolBSell) {
+      return null;
+    }
+    
+    let poolARates = {
+      buyTime: poolABuy.map(ph => new Date((ph.data.timestamp.seconds * 1000.0) + (ph.data.timestamp.nanoseconds / 1000000))),
+      buyRate: poolABuy.map(ph => ph.data.rate),
+      sellTime: poolASell.map(ph => new Date((ph.data.timestamp.seconds * 1000.0) + (ph.data.timestamp.nanoseconds / 1000000))),
+      sellRate: poolASell.map(ph => ph.data.rate)
+    }
+    let poolBRates = {
+      buyTime: poolBBuy.map(ph => new Date((ph.data.timestamp.seconds * 1000.0) + (ph.data.timestamp.nanoseconds / 1000000))),
+      buyRate: poolBBuy.map(ph => ph.data.rate),
+      sellTime: poolBSell.map(ph => new Date((ph.data.timestamp.seconds * 1000.0) + (ph.data.timestamp.nanoseconds / 1000000))),
+      sellRate: poolBSell.map(ph => ph.data.rate)
+    }
+
+    console.log(poolARates);
+    console.log(poolBRates);
+    
+    let estimatedProfits = [];
+    let estimatedTimes = [];
+
+    while (poolARates.buyTime.length > 0 && poolARates.sellTime.length > 0 && poolBRates.buyTime.length > 0 && poolBRates.sellTime.length > 0) {
+      let newestTime = new Date(Math.max.apply(null, [poolARates.buyTime[0], poolARates.sellTime[0], poolBRates.buyTime[0], poolBRates.sellTime[0]]));
+      let estimatedProfit = {
+        aThenB: (poolARates.buyRate[0] * poolBRates.sellRate[0]) - 1,
+        bThenA: (poolBRates.buyRate[0] * poolARates.sellRate[0]) - 1
+      }
+      // console.log("Estimated Profit", estimatedProfit, 
+      // "Rates", { 
+      //   "A then B": [poolARates.buyRate[0], poolBRates.sellRate[0]], 
+      //   "B then A": [poolBRates.buyRate[0], poolARates.sellRate[0]]
+      // });
+      estimatedProfits.push(Math.max(estimatedProfit.aThenB, estimatedProfit.bThenA));
+      estimatedTimes.push(newestTime);
+
+      if (newestTime.getTime() === poolARates.buyTime[0].getTime()) {
+        poolARates.buyTime.shift();
+        poolARates.buyRate.shift();
+      }
+      if (newestTime.getTime() === poolARates.sellTime[0].getTime()) {
+        poolARates.sellTime.shift();
+        poolARates.sellRate.shift();
+      }
+      if (newestTime.getTime() === poolBRates.buyTime[0].getTime()) {
+        poolBRates.buyTime.shift();
+        poolBRates.buyRate.shift();
+      }
+      if (newestTime.getTime() === poolBRates.sellTime[0].getTime()) {
+        poolBRates.sellTime.shift();
+        poolBRates.sellRate.shift();
+      }
+    }
+
+    console.log({poolARates, poolBRates, estimatedProfits});
+    console.log("Best possible trade:", Math.max.apply(null, estimatedProfits));
+    console.log("Worst possible trade:", Math.min.apply(null, estimatedProfits));
+
+    setProfitDatas({
+      profit: estimatedProfits,
+      time: estimatedTimes
+    });
   }
 
   return (
@@ -222,28 +282,27 @@ function Metrics() {
             rateDatas.map((data, index) => {
               if (data && rateDisplays[index]) {
                 let arrayData = {};
-                if (rateDisplays[index].yaxis === "y2") {
+                if (rateDisplays[index].yaxis === "y1") {
                   arrayData = {
                     type: "scatter",
                     mode: "lines+points",
                     name: rateDisplays[index].displayName,
                     secondary_y: rateDisplays[index].secondaryYAxis,
-                    x: data.map(ph => new Date(ph.data.timestamp.seconds * 1000)),
+                    x: data.map(ph => new Date((ph.data.timestamp.seconds * 1000) + (ph.data.timestamp.nanoseconds / 1000000))),
+                    y: data.map(ph => ph.data.rate)
+                  }
+                }
+                else if (rateDisplays[index].yaxis === "y2"){
+                  arrayData = {
+                    type: "scatter",
+                    mode: "lines+points",
+                    name: rateDisplays[index].displayName,
+                    secondary_y: rateDisplays[index].secondaryYAxis,
+                    x: data.map(ph => new Date((ph.data.timestamp.seconds * 1000) + (ph.data.timestamp.nanoseconds / 1000000))),
                     y: data.map(ph => ph.data.rate),
                     yaxis: "y2"
                   }
                 }
-                else {
-                  arrayData = {
-                    type: "scatter",
-                    mode: "lines+points",
-                    name: rateDisplays[index].displayName,
-                    secondary_y: rateDisplays[index].secondaryYAxis,
-                    x: data.map(ph => new Date(ph.data.timestamp.seconds * 1000)),
-                    y: data.map(ph => ph.data.rate)
-                  }
-                }
-                
                 return arrayData;
               } else {
                 return {
