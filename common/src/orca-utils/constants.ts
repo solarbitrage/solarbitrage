437a08c3e18b6928e67b2b38a4e9bdcf455c8ad1
George Thayamkery <george.thayamkery@gmail.com>
2022-04-11 14:12:34 -0500

    Multiple pool support

---
+++ common/src/orca-utils/constants.ts
@@ -0,0 +1,44 @@
+import { OrcaPoolConfig } from "@orca-so/sdk";
+import { orcaPoolConfigs } from "@orca-so/sdk/dist/constants";
+import { CurveType, OrcaPoolParams } from "@orca-so/sdk/dist/model/orca/pool/pool-types";
+import { PublicKey } from "@solana/web3.js";
+import { Percentage } from "@orca-so/sdk/dist/public/utils/models/percentage";
+import * as Tokens from "@orca-so/sdk/dist/constants/tokens";
+
+
+export const listeners: [OrcaPoolParams, string][] = [
+    [orcaPoolConfigs[OrcaPoolConfig.SOL_USDC], "ORCA_SOL_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.ETH_USDC], "ORCA_ETH_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.ORCA_USDC], "ORCA_ORCA_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.LIQ_USDC], "ORCA_LIQ_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.SNY_USDC], "ORCA_SNY_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.mSOL_USDC], "ORCA_mSOL_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.SLRS_USDC], "ORCA_SLRS_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.PORT_USDC], "ORCA_PORT_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.SBR_USDC], "ORCA_SBR_USDC"],
+    [orcaPoolConfigs[OrcaPoolConfig.RAY_USDC], "ORCA_RAY_USDC"],
+    [Object.freeze({
+        address: new PublicKey("6fTRDD7sYxCN7oyoSQaN1AWC3P2m8A6gVZzGrpej9DvL"),
+        nonce: 253,
+        authority: new PublicKey("B52XRdfTsh8iUGbGEBJLHyDMjhaTW8cAFCmpASGJtnNK"),
+        poolTokenMint: new PublicKey("ECFcUGwHHMaZynAQpqRHkYeTBnS5GnPWZywM8aggcs3A"),
+        poolTokenDecimals: 9,
+        feeAccount: new PublicKey("4pdzKqAGd1WbXn1L4UpY4r58irTfjFYMYNudBrqbQaYJ"),
+        tokenIds: [Tokens.solToken.mint.toString(), Tokens.usdcToken.mint.toString()],
+        tokens: {
+            [Tokens.solToken.mint.toString()]: {
+                ...Tokens.solToken,
+                addr: new PublicKey("FdiTt7XQ94fGkgorywN1GuXqQzmURHCDgYtUutWRcy4q"),
+            },
+            [Tokens.usdcToken.mint.toString()]: {
+                ...Tokens.usdcToken,
+                addr: new PublicKey("7VcwKUtdKnvcgNhZt5BQHsbPrXLxhdVomsgrr7k2N5P5"),
+            },
+        },
+        curveType: CurveType.ConstantProduct,
+        feeStructure: {
+            traderFee: Percentage.fromFraction(25, 10000),
+            ownerFee: Percentage.fromFraction(5, 10000),
+        },
+    }), "ORCA|6fTR_SOL_USDC"]
+]
--- common/src/raydium-utils/constants.ts
+++ common/src/raydium-utils/constants.ts
@@ -1,3 +1,5 @@
+import { LiquidityPoolJsonInfo } from "@raydium-io/raydium-sdk";
+
 export const RAYDIUM_POOLS_ENDPOINT = "https://sdk.raydium.io/liquidity/mainnet.json";
 
 export const listeners = [
@@ -11,17 +13,49 @@ export const listeners = [
   "6nJes56KF999Q8VtQTrgWEHJGAfGMuJktGb8x2uWff2u", // PORT-USDC
   "DudevotmDLN3KDHA1uTV1AyTYdwGnKUDFEXS9AXLjQ1z", // SBR-USDC
   "6UmmUiYoBjSrhakAobJw8BvkmJtDVxaeBtbt7rxWo1mg", // RAY-USDC
+  "HMDDVchYSP4uJBNFnWrNHg5rV2efy84TkVf733RPHm9k", // Alt: LIQ-USDC
+  "3gSjs6MqyHFsp8DXvaKvVUJjV7qg5itf9qmUGuhnSaWH"  // Alt: SOL-USDC
 ];
 
 export const poolMintAddrToName = {
-  "58oQChx4yWmvKdwLLZzBi4ChoCc2fqCUWBkwMihLYQo2": "SOL_USDC",
-  "EoNrn8iUhwgJySD1pHu8Qxm5gSQqLK3za4m8xzD2RuEb": "ETH_USDC",
-  "C5yXRTp39qv5WZrfiqoqeyK6wvbqS97oBqbsDUqfZyu": "ORCA_USDC",
-  "33dWwj33J3NUzoTmkMAUq1VdXZL89qezxkdaHdN88vK2": "LIQ_USDC",
-  "5TgJXpv6H3KJhHCuP7KoDLSCmi8sM8nABizP7CmYAKm1": "SNY_USDC",
-  "ZfvDXXUhZDzDVsapffUyXHj9ByCoPjP4thL6YXcZ9ix": "mSOL_USDC",
-  "7XXKU8oGDbeGrkPyK5yHKzdsrMJtB7J2TMugjbrXEhB5": "SLRS_USDC",
-  "6nJes56KF999Q8VtQTrgWEHJGAfGMuJktGb8x2uWff2u": "PORT_USDC",
-  "DudevotmDLN3KDHA1uTV1AyTYdwGnKUDFEXS9AXLjQ1z": "SBR_USDC",
-  "6UmmUiYoBjSrhakAobJw8BvkmJtDVxaeBtbt7rxWo1mg": "RAY_USDC",
+  "58oQChx4yWmvKdwLLZzBi4ChoCc2fqCUWBkwMihLYQo2": "RAYDIUM_SOL_USDC",
+  "EoNrn8iUhwgJySD1pHu8Qxm5gSQqLK3za4m8xzD2RuEb": "RAYDIUM_ETH_USDC",
+  "C5yXRTp39qv5WZrfiqoqeyK6wvbqS97oBqbsDUqfZyu": "RAYDIUM_ORCA_USDC",
+  "33dWwj33J3NUzoTmkMAUq1VdXZL89qezxkdaHdN88vK2": "RAYDIUM_LIQ_USDC",
+  "5TgJXpv6H3KJhHCuP7KoDLSCmi8sM8nABizP7CmYAKm1": "RAYDIUM_SNY_USDC",
+  "ZfvDXXUhZDzDVsapffUyXHj9ByCoPjP4thL6YXcZ9ix": "RAYDIUM_mSOL_USDC",
+  "7XXKU8oGDbeGrkPyK5yHKzdsrMJtB7J2TMugjbrXEhB5": "RAYDIUM_SLRS_USDC",
+  "6nJes56KF999Q8VtQTrgWEHJGAfGMuJktGb8x2uWff2u": "RAYDIUM_PORT_USDC",
+  "DudevotmDLN3KDHA1uTV1AyTYdwGnKUDFEXS9AXLjQ1z": "RAYDIUM_SBR_USDC",
+  "6UmmUiYoBjSrhakAobJw8BvkmJtDVxaeBtbt7rxWo1mg": "RAYDIUM_RAY_USDC",
+  "HMDDVchYSP4uJBNFnWrNHg5rV2efy84TkVf733RPHm9k": "RAYDIUM|HMDD_LIQ_USDC",
+  "3gSjs6MqyHFsp8DXvaKvVUJjV7qg5itf9qmUGuhnSaWH": "RAYDIUM|3gSj_SOL_USDC"
 };
+
+
+export const moreUnofficialLpPools: LiquidityPoolJsonInfo[] = [
+  {
+    id: "FyqYBBJ8vhr5AtDZiyJue4Khx9Be6Xijx5nm6aL6wZZV",
+    programId: "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8",
+    marketAuthority: "8BJwHZek2AmYVrsomc82aSAtod4r99fVhf3L5b8AtSrb",
+    marketQuoteVault: "2jT8622k6RyXkgJFvLmm7xoTrdLac9Aa6rZKP46w7E87",
+    marketBaseVault: "9JQsP7B8L4KiSakVDaWcDuSofUKnRweRchEnFNX8CvaB",
+    marketEventQueue: "7k6y9jPsmXRiK53q7hKmiLThxFt61HAuYS46vxXi9uji",
+    marketAsks: "H8b5qP1JSP7XAwMDpx1ptGz66Tf21xQWhTHsKmMV7jPu",
+    marketBids: "2UxFySzJ4TZ2UCmKj1jtTpBTXxYxzUkWYHo31MqJh1ac",
+    marketId: "3xGWh76WXVvxMWearhJJYtuN1NazD4eEaafAAnWXL757",
+    marketProgramId: "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin",
+    quoteVault: "8KKZ4LR5Q1T1SVQ2DaCfZ83NHr56152A5NMhDDVCCdVt",
+    baseVault: "79uBcM1XJWY7NT1UfydKH2jM7nepWSidJA2Khf3jfwm9",
+    targetOrders: "6YJqaJqVSMdcSdojokf8cwwpnQpdTGoGiWu2oYeCJMJ8",
+    openOrders: "2if3HMMqGZAKeETqAbPCxhy5obdhs2BbiMTqkGLQhRqw",
+    authority: "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1",
+    version: 4,
+    baseMint: "So11111111111111111111111111111111111111112",
+    quoteMint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    lpMint: "So11111111111111111111111111111111111111112",
+    lpVault: "So11111111111111111111111111111111111111112",
+    withdrawQueue: "22JaAuHiAe6meqT7n1SgXtkPc5STtNJqdqkVhNWNGJbd",
+    marketVersion: 3
+  }
+]
