2cba9de33954a9d133fa7b742c36de510d9ef118
ryan-hall-tx <ryan.hall.tx@gmail.com>
2022-04-14 17:19:43 -0500

    Created splash screen

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,31 +1,64 @@
-import React from "react";
-import { Container, Row, Col, Button, Image } from "react-bootstrap";
-import solanalogo from '../assets/solana-logo.png'
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import stockphoto from '../assets/stock_photo.png'
 
 function Dashboard() {
     return (
-        <div class="landing-page-jumbotron">
-            <div class="center">
-                <div class="d-flex p-2 align-items-center justify-content-center borderGrey">
-                    <Container fluid>
-                        <Row>
-                            <Col sm>
-                                <div class="landing-page-text">
-                                    <h1><strong>Solarbitrage</strong></h1>
-                                    <h2>An <strong>efficient</strong>, <strong>state of the art</strong> arbitrage bot powered by the Solana blockchain.</h2>
-                                    <h3>Finding opportunities between <strong>15</strong> cryptocurrencies on <strong>2</strong> automatic money makers.</h3>
-                                    <Button variant="primary">Learn more</Button>
-                                </div>
-                            </Col>
+        <div>
+            <div class='landing-page-jumbotron'>
+                <div class='center'>
+                    <div>
+                        <Container fluid>
+                            <Row className='align-items-center'>
+                                <Col lg> 
+                                    <div class='landing-page-text'>
+                                        <h1>
+                                            <strong>Blazing fast</strong>, <strong>no fee</strong> arbitraging.
+                                        </h1>
+                                        <h2>
+                                            <strong>15</strong> cryptocurrencies. <br></br> 
+                                            <strong>2</strong> automatic money makers. <br></br>
+                                            <strong>1</strong> platform.
+                                        </h2> 
+                                        <br></br>
+                                        <Button size='lg' variant='primary'>Learn more</Button>
+                                    </div>
+                                </Col>
 
-                            <Col sm>
-                                <img class="landing-page-image" src={solanalogo}></img>
+                                <Col lg>
+                                    <img class='landing-page-image d-none d-lg-block' alt='decorative' src={stockphoto}></img>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+            </div>
+
+            <div id="info-section" class="info-section">
+                <h2>
+                    Information
+                </h2>
+                <div class = 'borderGrey'>
+                    <Container>
+                        <Row className='align-items-center'>
+                            <Col md>
+                                Create an account
+                            </Col>
+                            <Col md>
+                                Deposit funds 
+                            </Col>
+                            <Col md>
+                                Start your trading bot
+                            </Col>
+                            <Col md>
+                                Monitor your profit
                             </Col>
                         </Row>
                     </Container>
                 </div>
             </div>
         </div>
+
     )
 }
 
