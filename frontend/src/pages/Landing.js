bd191aace63011b259cf221e0279ac3d56494f59
ryan-hall-tx <ryan.hall.tx@gmail.com>
2022-04-15 01:42:32 -0500

    Rough draft of landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card, CardGroup } from 'react-bootstrap';
+import Footer from '../components/footer'
+import FAQ from '../components/faq'
+import Team from '../components/team'
 import stockphoto from '../assets/stock_photo.png'
 
 function Dashboard() {
@@ -11,7 +14,7 @@ function Dashboard() {
                         <Container fluid>
                             <Row className='align-items-center'>
                                 <Col lg> 
-                                    <div class='landing-page-text'>
+                                    <div class='landing-page-text text-white'>
                                         <h1>
                                             <strong>Blazing fast</strong>, <strong>no fee</strong> arbitraging.
                                         </h1>
@@ -21,7 +24,9 @@ function Dashboard() {
                                             <strong>1</strong> platform.
                                         </h2> 
                                         <br></br>
-                                        <Button size='lg' variant='primary'>Learn more</Button>
+                                        <a href="#info-section">
+                                            <Button size='lg' variant='primary'>Learn more</Button>                      
+                                        </a>
                                     </div>
                                 </Col>
 
@@ -34,29 +39,60 @@ function Dashboard() {
                 </div>
             </div>
 
+            <Team />
+
             <div id="info-section" class="info-section">
-                <h2>
-                    Information
-                </h2>
-                <div class = 'borderGrey'>
+                <div>
                     <Container>
+                        <h2 className="text-white" style={{paddingTop: "25px", paddingBottom: "25px"}}>
+                            How does it work?
+                        </h2>
                         <Row className='align-items-center'>
-                            <Col md>
-                                Create an account
-                            </Col>
-                            <Col md>
-                                Deposit funds 
-                            </Col>
-                            <Col md>
-                                Start your trading bot
-                            </Col>
-                            <Col md>
-                                Monitor your profit
-                            </Col>
+                            <CardGroup>
+                                <Card className="mb-3">
+                                    <Card.Header as="h5">Step 1.</Card.Header>
+                                    <Card.Body>
+                                        <Card.Title>Create an Account</Card.Title>
+                                        <Card.Text>
+                                        Start your arbitraging experience by creating an account!
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                <Card className="mb-3">
+                                    <Card.Header as="h5">Step 2.</Card.Header>
+                                    <Card.Body>
+                                        <Card.Title>Deposit Funds</Card.Title>
+                                        <Card.Text>
+                                        By depositing funds into Solarbitrage, your bot will then have starting capital to begin trading with.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                <Card className="mb-3">
+                                    <Card.Header as="h5">Step 3.</Card.Header>
+                                    <Card.Body>
+                                        <Card.Title>Create Your Arbitrage Bot</Card.Title>
+                                        <Card.Text>
+                                        Go through a simple set up process to create your arbitrage bot.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                    <Card className="mb-3" >
+                                    <Card.Header as="h5">Step 4.</Card.Header>
+                                    <Card.Body>
+                                        <Card.Title>Watch Your Profits Grow</Card.Title>
+                                        <Card.Text>
+                                        Yes. It's really that simple!
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </CardGroup>
                         </Row>
                     </Container>
                 </div>
             </div>
+            
+            <FAQ />
+            <Footer />
         </div>
 
     )
